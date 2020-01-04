cb02a43ceece3afe56e6f8ed65adf4f53fcbb66e
Art B <rxarthurb@hotmail.com>
2020-01-04 14:27:35 -0500

    change main files add node module files

--- app.js
+++ app.js
@@ -11,15 +11,21 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res, next)=>{
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     var today = new Date();
-    var currentDay = today.getDay();
-    var days = {
-        1 : "Monday",
-        2 : "Tuesday",
-        3 : "Wednesday"
-    }
-    var theDay = days[currentDay];
-    res.render("index", {kindOfDay: theDay, newListItems: items});
+    var currentDay = today.toLocaleDateString("en-US", options);
+    // var days = {
+    //     1 : "Monday",
+    //     2 : "Tuesday",
+    //     3 : "Wednesday",
+    //     4 : "Thursday",
+    //     5 : "Friday",
+    //     6 : "Saturday",
+    //     7 : "Sunday"
+    // }
+    // var theDay = days[currentDay];
+
+    res.render("list", {kindOfDay: currentDay, newListItems: items});
     
 });
 
